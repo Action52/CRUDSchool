4e26eb9108682ea4f2e23d2fb1f171ecf592887c
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2017-03-22 18:46:41 -0600

    Index un poco más estético

--- school/index.php
+++ school/index.php
@@ -4,22 +4,50 @@
         <meta charset="utf-8">
         <title>Tutoring</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+        <style>
+          body{
+            background-image: url("img/wallpaper.jpg");
+          }
+          .container{
+            background-color: white;
+            border-radius: 25px;
+          }
+
+          .semitrans{
+            -moz-border-radius:30px;
+            -webkit-border-radius:30px;
+            border-radius:30px;
+            border: #solid 10px #000;
+            background-color: rgba(105,100,100,0.8);
+            width:80%;
+            margin-left: auto ;
+            margin-right: auto ;
+          }
+
+          .text-center{
+            color: #1bc626;
+          }
+
+          .link{
+            color: #1bc626;
+          }
+        </style>
     </head>
     <body>
-        <div class="container">
+        <div class="container semitrans">
             <div class="col-lg-12">
                 <h2 class="text-center text-primary">Tutoring</h2>
                 <div class="col-lg-1 pull-right" style="margin-bottom: 10px">
 
                 </div>
-                <br>
-                <a href = "p1.php">Define my available hours</a>
-                <br>
-                <br>
-                <a href = "app/list.php">Schedule an appointment</a>
-                <br>
-                <br>
-                <a href = "app/verification.php">View my appointments</a>
+            </div>
+            <div class="col-lg-12">
+                <h2 class="text-center text-primary pull-right"></h2>
+                <h4>
+                  <a href = "login.php" class = "link"> Login </a>
+                  |
+                  <a href = "register.php" class = "link"> Register </a>
+                </h4>
             </div>
         </div>
     </body>
