e22e89af1c095a8c1dba701736e44102474f2c7d
Luis Alfredo León Villapún <leonvillapun@gmail.com>
2017-03-25 21:05:24 -0600

    Se agregó index chingón

---
+++ school/index2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SCHOOL</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+</head>
+<body>
+    <div class="container">
+        <div class="col-lg-12">
+
+            <h2 class="text-center text-primary">Login</h2>
+            <form action="login.php" method="post">
+            Usuario:<input type="text" class = "form-control" name="username" size="20" maxlength="20" />
+            <br />
+            Password:<input type="password" class = "form-control" name="password" size="10" maxlength="10" />
+            <br />
+            <input type="submit" value="Ingresar" />
+            </form>
+
+        </div>
+    </div>
+
+
+</body>
+</html>
